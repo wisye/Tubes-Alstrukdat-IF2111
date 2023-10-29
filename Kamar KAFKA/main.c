c4d6d7e8adfa5a12bc95ebbbb4de57056a7375dd
wiswis <lunarwisyendra@gmail.com>
2023-10-29 18:20:17 +0700

    testing in main

--- Kamar KAFKA/main.c
+++ Kamar KAFKA/main.c
@@ -12,23 +12,28 @@ boolean stringComp(char *x, char *y){
     return true;
 }
 
-void startspotify(){
-    START();
-}
-
 void help(){
     printf("START : Memulai spotify\n");
 }
 
 int main(){
     char *command;
+
+    printf(">> ");
     scanf("%s", command);
     
     if(stringComp(command, "START")){
         startspotify();
     }
+    else if(stringComp(command, "LOAD")){
+        loadspotify();
+    }
+
     else if(stringComp(command, "HELP")){
         help();
+    }
+    else if(stringComp(command, "EXIT")){
+        exitspotify();
     }
     else{
         printf("invalid command");
