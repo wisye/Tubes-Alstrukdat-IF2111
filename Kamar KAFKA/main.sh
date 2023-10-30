c59b546e8e2315c71dc7ff189a26f430aba93aed
Wisyendra Lunarmalam <123868853+wisye@users.noreply.github.com>
2023-10-30 18:35:54 +0700

    update main.sh

--- Kamar KAFKA/main.sh
+++ Kamar KAFKA/main.sh
@@ -1,5 +1,5 @@
 # CURRENTLY FOR TESTING DRIVERS
 
-gcc  "src/ADT_C/mesinkarakter.c" "src/ADT_C/mesinkata.c" "src/command/console.c" "src/command/consoleDriver.c" -o main
+gcc  "src/ADT_C/mesinkarakter.c" "src/ADT_C/mesinkata.c" "src/command/console.c" "main.c" -o main
 ./main
 # "src/ADT_C/list.c" "src/ADT_C/queue.c" "src/ADT_C/stack.c"
