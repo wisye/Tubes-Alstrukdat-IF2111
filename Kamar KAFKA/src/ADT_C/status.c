ccc9706387173e18fedbc526fb31f789b29dddd6
EleanorCordelia <116582118+EleanorCordelia@users.noreply.github.com>
2023-11-13 11:28:10 +0700

    Update status.c

--- Kamar KAFKA/src/ADT_C/status.c
+++ Kamar KAFKA/src/ADT_C/status.c
@@ -2,6 +2,20 @@
 
 #include "../ADT_H/globalvar.h"
 
-int main(){
-    return 0;
+void showStatus(){
+    // asumsikan menerima data bersih dari var global.
+
+    printf("Now Playing:\n");
+    if(STATUS){
+        // show songs
+
+    } else {
+        printf("No songs have been played yet. Please search for a song to begin playback.\n");
+    }
+
+    // print queue
+    printf("Queue:\n")
+
+    // cek dari variable global Queue nya.
+
 }
