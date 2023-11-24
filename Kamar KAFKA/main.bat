d9d66193d7485f7c7e6d3375a5363ce295282062
Wisyendra Lunarmalam <123868853+wisye@users.noreply.github.com>
2023-11-24 13:32:33 +0700

    Update main.bat

--- Kamar KAFKA/main.bat
+++ Kamar KAFKA/main.bat
@@ -1,2 +1,2 @@
-gcc "main.c" "src/ADT_C/moreADT.c" "src/ADT_C/mesinkarakter.c" "src/ADT_C/mesinkata.c" "src/ADT_C/queue.c" "src/ADT_C/stack.c" "src/ADT_C/list.c" "src/command/console.c" -o main.exe
+gcc "main.c" "welcome.c" "src/ADT/Playlist/playlist.c" "src/ADT/globalvar/globalvar.c" "src/ADT/func/func.c" "src/ADT/ListPlaylistDinamis/listPlaylist.c" "src/ADT/List/list.c" "src/ADT/Mesinkata/mesinkata.c" "src/ADT/Mesinkarakter/mesinkarakter.c" "src/ADT/Queue/queue.c" "src/ADT/Stack/stack.c" "src/command/console.c" -o main.exe
 main.exe
