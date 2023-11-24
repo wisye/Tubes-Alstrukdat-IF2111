7edbfa6cf85cd6eba4895b1ddd3f637c48001522
Harryts1 <118546322+Harryts1@users.noreply.github.com>
2023-11-24 13:21:24 +0700

    Update main.sh

--- Kamar KAFKA/main.sh
+++ Kamar KAFKA/main.sh
@@ -1,5 +1,5 @@
 # CURRENTLY FOR TESTING DRIVERS
 
 gcc "main.c" "welcome.c" "src/ADT/Playlist/playlist.c" "src/ADT/globalvar/globalvar.c" "src/ADT/func/func.c" "src/ADT/ListPlaylistDinamis/listPlaylist.c" "src/ADT/List/list.c" "src/ADT/Mesinkata/mesinkata.c" "src/ADT/Mesinkarakter/mesinkarakter.c" "src/ADT/Queue/queue.c" "src/ADT/Stack/stack.c" "src/command/console.c" -g -o main
-./main test.txt
+./main
 # "src/ADT_C/list.c" "src/ADT_C/queue.c" "src/ADT_C/stack.c"
